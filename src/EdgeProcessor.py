81d2fe7dc7ec679bdcbda87f27d60a62dacf62f5
RohitBagda <rbagda@macalester.edu>
2019-04-18 12:44:22 -0500

    Removed Comments

--- src/EdgeProcessor.py
+++ src/EdgeProcessor.py
@@ -21,18 +21,3 @@ class EdgeProcessor:
             else:
                 edges_with_num_players_as_weights[edge] = 1
         return edges_with_num_players_as_weights
-
-
-# edges = []
-# edge1 = Edge("team1", "team2", 129)
-# edges.append(edge1)
-# edge2 = Edge("team1", "team2", 300)
-# edges.append(edge2)
-# edge3 = Edge("team2", "team1", 200)
-# edges.append(edge3)
-
-# print(float("$3.2k"))
-
-# weights = EdgeProcessor.show_edges_with_num_players_as_weights(edges)
-# for w in weights:
-#     print(str(w), weights[w])
--- src/dataScrapper.py
+++ src/dataScrapper.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 from TransferLink import *
 from EdgeProcessor import *
 from DataOrganiser import *
@@ -42,28 +41,8 @@ for league_name in league_name_url_matcher.league_name_url_map:
 
         tuples = []
         for box in club_boxes:
-            # if box:
-            #     club = box.find_all("div", {"class": "table-header"})
-            #     if club:
-            #         print(club)
-            #     club_name = club.select("a", {"class": "vereinprofil_tooltip"})[1].get_text()
-            #     # print(club_name)
-            #     club_details = box.find_all("div", {"class": "responsive-table"})
-            #     if club_details:
-            #         out_transfers = club_details[1]
-            #         print(out_transfers)
-            #         print("=================================")
-
-            # table_headers.append(box.find_all("div", {"class": "table-header"}))
-            # table_div.append(box.find_all("div", {"class": "responsive-table"}))
             header_div_tuple = (box.find_all("div", {"class": "table-header"}), box.find_all("div", {"class": "responsive-table"}))
             tuples.append(header_div_tuple)
-        #
-        # table_headers = table_headers[2:]
-        # for table_header in table_headers:
-        #     if table_header:
-        #         selling_club = table_header[0].select("a", {"class": "vereinprofil_tooltip"})[1].get_text()
-        #         print(selling_club)
 
         for t in tuples:
             if t[0] and t[1]:
@@ -71,9 +50,6 @@ for league_name in league_name_url_matcher.league_name_url_map:
                 club = t[0][0].select("a", {"class": "vereinprofil_tooltip"})[1]
                 club_name = club.get_text()
                 club_id = club.get("id")
-                # print(club_id)
-                # club_id = t[0][0].select("a", )
-                # print(club_name + "\n" + "-------------")
                 table_div = t[1]
                 if table_div:
                     transfers_out = t[1][1]
@@ -87,12 +63,9 @@ for league_name in league_name_url_matcher.league_name_url_map:
                             target_team_id = target_team.get("id")
                             link = TransferLink(source_team=club_id, target_team=target_team_id)
                             league.transfers_for_year[year].append(link)
-                            # league_edges.append(edge)
-                            # all_edges.append(edge)
+
         print("------Finished calculating data for " + year_as_string + "----------")
     all_leagues.append(league)
-    # league_edge_processor = EdgeProcessor()
-    # league_weights
 
 edge_processor = EdgeProcessor()
 
@@ -102,65 +75,3 @@ for league in all_leagues:
         print("=======showing the links for " + str(year))
         for link in league.transfers_for_year[year]:
             link.show_info()
-        # all_links_for_year = edge_processor.show_edges_with_num_players_as_weights(list_edges=league.edges_for_each_year[year])
-        # for link in all_links_for_year:
-        #     print(link.source_team, link.target_team, all_links_for_year[link])
-
-                    # print(player_name + "," + str(target_team))
-                # all_transfers_out = transfers_out.find_all("span", {"class": "hide-for-small"})
-                # ages = transfers_out.find_all("td", {"class": "zentriert alter-transfer-cell"})
-                # for transfer_out in all_transfers_out:
-                #     print(transfer_out.get_text())
-
-            # print("===================================================================================================")
-# edge_processor = EdgeProcessor()
-# weights = edge_processor.show_edges_with_num_players_as_weights(list_edges=edges)
-# with open(file_name, "w+") as file:
-#     file.truncate()
-#     for w in weights:
-#         file.write(str(w) + "," + str(weights[w]) + "\n")
-
-# for w in weights:
-#     print(str(w) + "," + str(weights[w]))
-# print(len(edges))
-# for edge in edges:
-#     print(str(edge))
-
-
-# table_headers = table_headers[2:]
-# table_div = [table for table in table_div if table]
-#
-# table_rows = []
-# table_data = []
-# for thing in table_div[0]:
-#     table_rows.append(thing.find_all("tr"))
-#     for row in table_rows[0]:
-#         table_data.append(row.find_all("td"))
-#
-# for
-# table_data = [data for data in table_data if data]
-# for data in table_data:
-#     print(data[0].find("span", {"class": "hide-for-small"}).get_text())
-#     print(data[1].get_text())
-#     print(data[2])
-#     print("+++++++++++++")
-# for table_header in table_headers:
-#     if table_header:
-#         selling_clubs.append(table_header[0].select("a", {"class": "vereinprofil_tooltip"})[1].get_text())
-#
-# print(selling_clubs)
-# new_table = pd.DataFrame(columns=range(0, 7), index=[0])  # I know the size
-
-
-
-# for each_table_div in table_div:
-# row_marker = 0
-# for thing in table_div[0]:
-#     for row in thing.find_all("tr"):
-#         column_marker = 0
-#         columns = row.find_all('td')
-#         for column in columns:
-#             new_table.iat[row_marker, column_marker] = column.get_text()
-#             column_marker += 1
-
-# print(new_table)
