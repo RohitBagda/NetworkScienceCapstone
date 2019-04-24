083087542d870eca63d939480fd2280562fd8bd9
hogeleka <hogeleka@macalester.edu>
2019-04-24 11:54:43 -0500

    updated the filename of generated csv files

---
+++ src/LeagueURLConstants.py
@@ -0,0 +1,10 @@
+class LeagueURLConstants:
+    BUNDESLIGA = "Bundesliga"
+    BUNDESLIGA_URL = "bundesliga/transfers/wettbewerb/L1"
+    EPL = "EPL"
+    EPL_URL = "premier-league/transfers/wettbewerb/GB1"
+
+    league_names_with_urls = {
+        BUNDESLIGA: BUNDESLIGA_URL,
+        EPL: EPL_URL
+    }
