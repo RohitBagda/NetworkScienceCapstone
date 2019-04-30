0247c458681d8a6eb9457a44f4d26370ea9841f5
RohitBagda <rbagda@macalester.edu>
2019-04-29 23:28:33 -0500

    Added League Income and Expenditure Per Year Data

--- src/WebscrapingProgram.py
+++ src/WebscrapingProgram.py
@@ -3,7 +3,6 @@ import time
 
 def main():
     start = time.time()
-    # try:
     scraper_tool = TransferDataScraperAndProcessor(start_year=2000, end_year=2018)
     scraper_tool.write_out_all_clubs_and_names()
     scraper_tool.write_output_file_for_loans()
@@ -12,10 +11,6 @@ def main():
     scraper_tool.write_output_file_for_non_loan_transfers(use_amount_as_weight=True)
     scraper_tool.write_output_file_for_non_loan_transfers(use_amount_as_weight=False)
     scraper_tool.write_out_clubs_and_countries()
-
-    # except Exception as e:
-    #     print("=============something went wrong somewhere===============")
-    #     print(e)
     end = time.time()
     print(str(end - start) + " seconds")
 
